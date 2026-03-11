683d6740068cf621e660171aaec7ef28a5f4e8e1
emellop-senai <edio.pereira@edu.sc.senai.br>
2026-03-11 17:08:50 -0300

    create get route and use it

---
+++ 11-03-2026/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let alunos = [
+    { id: 1, nome: "Édio", idade: 35 },
+    { id: 2, nome: "Vanessa", idade: 52 },
+    { id: 3, nome: "Schuster", idade: 13 }
+]
+
+app.get('/', (req, res) => {
+    res.send('Vanessa Schuster')
+})
+
+app.get('/alunos/getAll', (req, res) => {
+    res.json({
+        sucess: true,
+        data: alunos
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
