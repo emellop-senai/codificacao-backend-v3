4a44deffcc34298d6697bf765f04c51369adc1c4
emellop-senai <edio.pereira@edu.sc.senai.br>
2026-04-22 15:35:12 -0300

    finalizado revisão de fs

---
+++ 20-04-2026/atv-fs/index.js
@@ -0,0 +1,27 @@
+import fs from "fs/promises"
+
+// JSON FILE
+
+async function readFruits() {
+    const data = await fs.readFile("./fruits.json", "utf-8")
+    const fruits = JSON.parse(data)
+    return fruits;
+}
+
+// Mozile mdn
+async function writeFruits(params) {
+    const data = JSON.stringify(fruits, null, 2)
+    await fs.writeFile("./fruits.json", data, "utf-8")
+
+}
+
+// const fruits = await readFruits()
+// fruits.push({ id: 4, nome: "Naranja" })
+// await writeFruits(fruits)
+
+// APIII
+
+async function getAll() {
+    const fruits = await readFruits()
+    return fruits
+}
